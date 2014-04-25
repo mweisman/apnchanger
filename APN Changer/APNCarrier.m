4804f1fe3a370da0a683acc5b42d792e57c1ce44
Michael Weisman <mweisman@gmail.com>
2014-04-24 19:29:09 -0700

    XML escape input

--- APN Changer/APNCarrier.m
+++ APN Changer/APNCarrier.m
@@ -21,21 +21,39 @@
 
 - (NSString *) carrierXML
 {
+    NSString* (^xml_escape)(NSString*) = ^NSString*(NSString* valueString) {
+        NSString *s = [NSString stringWithString:valueString];
+        s = [s stringByReplacingOccurrencesOfString:@"\"" withString:@"&quot;"];
+        s = [s stringByReplacingOccurrencesOfString:@"'" withString:@"&apos;"];
+        s = [s stringByReplacingOccurrencesOfString:@"&" withString:@"&amp;"];
+        s = [s stringByReplacingOccurrencesOfString:@"<" withString:@"&lt;"];
+        s = [s stringByReplacingOccurrencesOfString:@">" withString:@"&gt;"];
+        return s;
+    };
+    
     NSString *xml;
     
     NSError *templateReadError;
     NSString *xmlLoc = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"template.mobileconfig"];
     NSString *templateXML = [NSString stringWithContentsOfFile:xmlLoc encoding:NSUTF8StringEncoding error:&templateReadError];
     
-    xml = [templateXML stringByReplacingOccurrencesOfString:@"$$CARRIERDESC$$" withString:self.carrierDescription];
-    xml = [xml stringByReplacingOccurrencesOfString:@"$$CARRIERNAME$$" withString:self.carrierName];
-    xml = [xml stringByReplacingOccurrencesOfString:@"$$APN$$" withString:self.carrierAPN];
+    NSString *escapedDesc =  xml_escape(self.carrierDescription);
+    xml = [templateXML stringByReplacingOccurrencesOfString:@"$$CARRIERDESC$$" withString:escapedDesc];
+    
+    NSString *escapedName =  xml_escape(self.carrierName);
+    xml = [xml stringByReplacingOccurrencesOfString:@"$$CARRIERNAME$$" withString:escapedName];
+    
+    NSString *escapedApn =  xml_escape(self.carrierAPN);
+    xml = [xml stringByReplacingOccurrencesOfString:@"$$APN$$" withString:escapedApn];
+    
     if (self.apnUsername) {
-        NSString *userXML = [NSString stringWithFormat:@"<key>username</key><string>%@</string>",self.apnUsername];
+        NSString *escapedUser =  xml_escape(self.apnUsername);
+        NSString *userXML = [NSString stringWithFormat:@"<key>username</key><string>%@</string>", escapedUser];
         xml = [xml stringByReplacingOccurrencesOfString:@"$$USER$$" withString:userXML];
     }
     if (self.apnPassword) {
-        NSString *userXML = [NSString stringWithFormat:@"<key>password</key><string>%@</string>",self.apnPassword];
+        NSString *escapedPass =  xml_escape(self.apnPassword);
+        NSString *userXML = [NSString stringWithFormat:@"<key>password</key><string>%@</string>",escapedPass];
         xml = [xml stringByReplacingOccurrencesOfString:@"$$PASS$$" withString:userXML];
     }
     xml = [xml stringByReplacingOccurrencesOfString:@"$$UUID1$$" withString:[[NSUUID UUID] UUIDString]];
